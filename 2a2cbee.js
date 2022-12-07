935776e037c817d708dfd300e4154f37bf55a54d
Connor Love <92341935+Coding-with-love@users.noreply.github.com>
2022-12-07 12:08:59 -0500

    Add files via upload

---
+++ 2a2cbee.js
@@ -0,0 +1,31 @@
+(window.webpackJsonp = window.webpackJsonp || []).push([
+    [36], {
+        404: function(t, e, n) {
+            "use strict";
+            n.r(e);
+            var c = n(15),
+                component = Object(c.a)({}, (function() {
+                    var t = this,
+                        e = t.$createElement;
+                    t._self._c;
+                    return t._m(0)
+                }), [function() {
+                    var t = this,
+                        e = t.$createElement,
+                        n = t._self._c || e;
+                    return n("div", {
+                        staticClass: "page-article article",
+                        attrs: {
+                            "data-scroll-content": "",
+                            "data-page": "article"
+                        }
+                    }, [n("header", {
+                        staticClass: "header"
+                    }, [n("h1", {
+                        staticClass: "e-sans"
+                    }, [t._v("uid")])])])
+                }], !1, null, null, null);
+            e.default = component.exports
+        }
+    }
+]);
